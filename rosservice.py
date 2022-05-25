3ee2713cd1767412358bdcc679e019c988ee9c8f
Amadeus-Winarto <amadeusw6@gmail.com>
2022-05-25 23:14:24 +0800

    Add ros service wrappers

---
+++ rosservice.py
@@ -0,0 +1,249 @@
+from threading import Thread, Lock
+import numpy as np
+import logging
+import copy
+import cv2
+
+import rospy
+from cv_bridge import CvBridge
+
+from typing import List, Dict, Union
+from wrappers.superglue import SuperGlueMatcher
+from wrappers.superpoint import SuperPointDetector
+
+from sensor_msgs.msg import CompressedImage
+from superglue_ros.msg import Keypoint
+from superglue_ros.srv import RegisterImage, RegisterImageRequest, RegisterImageResponse
+from superglue_ros.srv import ClearBuffer, ClearBufferRequest, ClearBufferResponse
+from superglue_ros.srv import MatchImages, MatchImagesRequest, MatchImagesResponse
+
+
+class ImageWrapper:
+    def __init__(self, id: int, img: np.ndarray) -> None:
+        self.id = id
+        self.img = img
+        self.thread = None
+        self.results = None
+
+    def register_thread(self, thread: Thread):
+        self.thread = thread
+
+    def add_results(self, results: Dict):
+        self.results = results
+
+
+class Buffer:
+    def __init__(self) -> None:
+        self.buffer: List[ImageWrapper] = []
+        self.lock = Lock()
+
+    def append(self, x) -> None:
+        self.lock.acquire()
+        self.buffer.append(x)
+        self.lock.release()
+
+    def clear(self) -> None:
+        self.lock.acquire()
+        self.buffer = []
+        self.lock.release()
+
+    def __len__(self) -> int:
+        self.lock.acquire()
+        length = len(self.buffer)
+        self.lock.release()
+        return length
+
+
+class MatcherNode:
+    """
+    ROSWrapper to offer matching services
+    """
+
+    def __init__(
+        self,
+        node_name="superglue_matcher",
+        detector_config: Union[Dict, None] = None,
+        matcher_config: Union[Dict, None] = None,
+    ):
+        rospy.init_node(node_name)
+        self.services: List[rospy.Service] = []
+        self.bridge = CvBridge()
+
+        self.detector = SuperPointDetector(detector_config)
+        self.matcher = SuperGlueMatcher(matcher_config)
+
+        self.buffer = Buffer()
+        self.lock = Lock()
+        self.idx = 0
+
+        self.debug_pub = rospy.Publisher("debug", CompressedImage)
+
+        self.offer_services()
+
+    def offer_services(self):
+        # Add the services here
+        self.services.append(
+            rospy.Service("registerImage", RegisterImage, self.register_img)
+        )
+        self.services.append(
+            rospy.Service("clearBuffer", ClearBuffer, self.clear_buffer)
+        )
+        self.services.append(
+            rospy.Service("inferMatches", MatchImages, self.infer_images)
+        )
+
+    def _get_keypoints(self, id: int):
+        self.buffer.lock.acquire()
+        relevant = [x for x in self.buffer.buffer if x.id == id]
+        if len(relevant) != 1:
+            self.buffer.lock.release()
+            raise ValueError("Impossible!")
+        else:
+            img = copy.deepcopy(relevant[0].img)
+            self.buffer.lock.release()
+
+        kp = self.detector(img)
+
+        self.buffer.lock.acquire()
+        for x in self.buffer.buffer:
+            if x.id == id:
+                x.add_results(kp)
+        self.buffer.lock.release()
+
+    def _add_img(self, img: np.ndarray) -> RegisterImageResponse:
+        response = RegisterImageResponse()
+        if len(self.buffer) < 2:
+            self.idx += 1
+            content = ImageWrapper(self.idx, img)
+            self.buffer.append(content)
+            response.result = 0  # No issues
+
+            # Get keypoints
+            t = Thread(target=self._get_keypoints, args=(self.idx,))
+            t.start()
+            content.register_thread(t)
+        else:
+            response.result = 1  # Buffer overflow
+        return response
+
+    def _infer_matches(self, num_keypoints: int):
+        if len(self.buffer) < 2:
+            raise ValueError("No matching possible!")
+        elif len(self.buffer) > 2:
+            logging.warning("More than 1 item in buffer. Picking first 2...")
+
+        self.buffer.lock.acquire()
+        content1 = self.buffer.buffer[0]
+        content2 = self.buffer.buffer[1]
+        self.buffer.lock.release()
+
+        content1.thread.join()  # type: ignore
+        content2.thread.join()  # type: ignore
+
+        keypoints = {"ref": content1.results, "cur": content2.results}
+        matches = self.matcher(keypoints, num_keypoints)
+
+        self.buffer.lock.acquire()
+        content1 = self.buffer.buffer.pop(0)
+        content2 = self.buffer.buffer.pop(0)
+        self.buffer.lock.release()
+
+        if self.debug_pub is not None:
+            img1 = content1.img
+            img2 = content2.img
+
+            from tools.tools import plot_matches
+
+            img = plot_matches(
+                cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY),
+                cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY),
+                matches["ref_keypoints"],
+                matches["cur_keypoints"],
+                matches["match_score"],
+                layout="lr",
+            )
+            cv2.imshow("MATCHES", img)
+            cv2.waitKey(0)
+
+            debug_img = self.bridge.cv2_to_compressed_imgmsg(img)
+            self.debug_pub.publish(debug_img)
+        return matches
+
+    def _to_correct_format(self, results: Dict) -> MatchImagesResponse:
+        response = MatchImagesResponse()
+
+        for k in results["ref_keypoints"]:
+            kp = Keypoint()
+            kp.coord = k
+            response.ref_keypoints.append(kp)
+
+        for k in results["cur_keypoints"]:
+            kp = Keypoint()
+            kp.coord = k
+            response.cur_keypoints.append(kp)
+        response.match_score = results["match_score"]  # List of floats
+        return response
+
+    def register_img(self, req: RegisterImageRequest) -> RegisterImageResponse:
+        topic_name = req.topic_name
+        img: CompressedImage = rospy.wait_for_message(
+            topic_name, CompressedImage, timeout=2
+        )
+        cv2_img: np.ndarray = self.bridge.compressed_imgmsg_to_cv2(img)
+        return self._add_img(cv2_img)
+
+    def clear_buffer(self, _: ClearBufferRequest) -> ClearBufferResponse:
+        response = ClearBufferResponse()
+        if len(self.buffer) < 0:
+            response.result = 1  # Buffer underflow
+        else:
+            self.buffer.clear()
+            response.result = 0
+        return response
+
+    def infer_images(self, request: MatchImagesRequest) -> MatchImagesResponse:
+        num_keypoints = request.numKeypoints
+        if num_keypoints <= 0:
+            resp = MatchImagesResponse()
+            resp.result = 1
+            return resp
+
+        results = self._infer_matches(num_keypoints)
+        return self._to_correct_format(results)
+
+    @staticmethod
+    def threaded_test():
+        import cv2
+
+        matcher_node = MatcherNode()
+        img1 = cv2.imread("/home/amadeus/bbauv/src/stereo/imgs/img1.jpg")
+        resp1 = matcher_node._add_img(img1)
+        print("Result1 : ", resp1.result)
+
+        img2 = cv2.imread("/home/amadeus/bbauv/src/stereo/imgs/img2.jpg")
+        matcher_node._add_img(img2)
+
+        assert len(matcher_node.buffer) == 2
+
+        matches = matcher_node._infer_matches(10)
+
+        from tools.tools import plot_matches
+
+        img = plot_matches(
+            cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY),
+            cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY),
+            matches["ref_keypoints"][0:200],
+            matches["cur_keypoints"][0:200],
+            matches["match_score"][0:200],
+            layout="lr",
+        )
+
+        cv2.imshow("MATCHES", img)
+        cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+    # MatcherNode.threaded_test()
+    matcher_node = MatcherNode()
+    print("RUNNING")
+    rospy.spin()
---
+++ wrappers/superpoint.py
@@ -0,0 +1,62 @@
+import logging
+import os
+
+import cv2
+import torch
+import numpy as np
+
+from models.SuperGluePretrainedNetwork.models.superpoint import SuperPoint
+from tools.tools import image2tensor
+
+from typing import Dict
+
+
+class SuperPointDetector(object):
+    default_config = {
+        "descriptor_dim": 256,
+        "nms_radius": 4,
+        "keypoint_threshold": 0.005,
+        "max_keypoints": -1,
+        "remove_borders": 4,
+        "path": os.path.abspath(os.path.join(__file__, "../.."))
+        + "/models/SuperPointPretrainedNetwork/superpoint_v1.pth",
+        "cuda": True,
+    }
+
+    def __init__(self, config=None):
+        if config is None:
+            config = {}
+
+        self.config = self.default_config
+        self.config = {**self.config, **config}
+        logging.info("SuperPoint detector config: ")
+        logging.info(self.config)
+
+        self.device = (
+            "cuda" if torch.cuda.is_available() and self.config["cuda"] else "cpu"
+        )
+
+        logging.info("creating SuperPoint detector...")
+        self.superpoint = SuperPoint(self.config).to(self.device)
+
+    def __call__(self, image) -> Dict:
+        try:
+            if image.shape[2] == 3:
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        except Exception as e:
+            logging.error(e)
+            pass  # Squeezed gray image array
+
+        logging.debug("detecting keypoints with superpoint...")
+        image_tensor = image2tensor(image, self.device)
+        pred = self.superpoint({"image": image_tensor})
+
+        ret_dict = {
+            "image_size": np.array([image.shape[0], image.shape[1]]),
+            "torch": pred,
+            "keypoints": pred["keypoints"][0].cpu().detach().numpy(),
+            "scores": pred["scores"][0].cpu().detach().numpy(),
+            "descriptors": pred["descriptors"][0].cpu().detach().numpy().transpose(),
+        }
+
+        return ret_dict
