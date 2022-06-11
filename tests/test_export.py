c341c5389c5d60517d6d4c716abc063be729658d
Amadeus <amadeusw6@gmail.com>
2022-06-11 17:09:43 +0800

    Update torchscript export test

--- tests/test_export.py
+++ tests/test_export.py
@@ -1,9 +1,18 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), ".."))
+
 from models.SuperGluePretrainedNetwork.models.superpoint import SuperPoint
 from models.SuperGluePretrainedNetwork.models.superglue import SuperGlue
-import os
 
 import torch
 import torch.onnx
+
+file_path = os.path.dirname(os.path.abspath(__file__))
+weight_path = os.path.join(
+    file_path, "..", "models", "SuperGluePretrainedNetwork", "models", "weights"
+)
 
 superpoint_config = {
     "descriptor_dim": 256,
@@ -11,15 +20,18 @@ superpoint_config = {
     "keypoint_threshold": 0.005,
     "max_keypoints": -1,
     "remove_borders": 4,
-    "path": os.path.abspath(os.path.join(__file__, "../.."))
-    + "/models/SuperGluePretrainedNetworks/models/weights/superpoint_v1.pth",
+    "path": os.path.join(
+        file_path,
+        "..",
+        "models/SuperGluePretrainedNetworks/models/weights/superpoint_v1.pth",
+    ),
     "cuda": True,
 }
 ts_superpoint = SuperPoint(superpoint_config).eval().cuda()
 
 superglue_config = {
     "descriptor_dim": 256,
-    "weights": "indoor",
+    "weights": "outdoor",
     "keypoint_encoder": [32, 64, 128, 256],
     "GNN_layers": ["self", "cross"] * 9,
     "sinkhorn_iterations": 100,
@@ -29,11 +41,8 @@ superglue_config = {
 ts_superglue = SuperGlue(superglue_config).eval().cuda()
 print("Loaded")
 
-torch.jit.save(
-    ts_superpoint,
-    "/home/amadeus/bbauv/src/SuperGlue_ROS/models/SuperGluePretrainedNetwork/models/weights/superpoint_v1.zip",
-)
+torch.jit.save(ts_superpoint, os.path.join(weight_path, "superpoint_v1.zip"))
 torch.jit.save(
     ts_superglue,
-    f"/home/amadeus/bbauv/src/SuperGlue_ROS/models/SuperGluePretrainedNetwork/models/weights/superglue_{superglue_config['weights']}.zip",
+    os.path.join(weight_path, f"superglue_{superglue_config['weights']}.zip"),
 )
